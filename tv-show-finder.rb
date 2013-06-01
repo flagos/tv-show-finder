9f60fc12e10477af44309471aa8eb0c41fbdc047
flagos <flagospub@gmail.com>
2013-06-01 16:50:29 +0200

    add licence

--- tv-show-finder.rb
+++ tv-show-finder.rb
@@ -1,5 +1,14 @@
 #!/usr/bin/env ruby
 
+
+# ----------------------------------------------------------------------------
+# "THE BEER-WARE LICENSE"
+# flagospub@gmail.com wrote this file. As long as you retain this notice you
+# can do whatever you want with this stuff. If we meet some day, and you think
+# this stuff is worth it, you can buy me a beer in return 
+# ----------------------------------------------------------------------------
+
+
 require 'micro-optparse'
 require 'rb-inotify'
 
