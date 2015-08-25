79fba35d0f1f1b4201d622c472b1d7265c1a72cb
flagos <flagos@users.noreply.github.com>
2015-08-25 17:38:27 +0200

    Update tv-show-finder.rb

--- tv-show-finder.rb
+++ tv-show-finder.rb
@@ -3,7 +3,7 @@
 
 # ----------------------------------------------------------------------------
 # "THE BEER-WARE LICENSE"
-# flagospub@gmail.com wrote this file. As long as you retain this notice you
+# I wrote this file. As long as you retain this notice you
 # can do whatever you want with this stuff. If we meet some day, and you think
 # this stuff is worth it, you can buy me a beer in return 
 # ----------------------------------------------------------------------------
